3f14dc2a8f7bfa87feddf344eceaf57998fbc5b2
Justin Du <justin.d128@gmail.com>
2016-01-23 19:18:27 -0600

    Deprecate and empty php entry point
    Change-Id: I4116aac773d60f20dfb225aeb360537757606bc2

--- Tempo.php
+++ Tempo.php
@@ -18,37 +18,15 @@
  * require_once "$IP/skins/Tempo/Tempo.php";
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	exit;
+if ( function_exists( 'wfLoadSkin' ) ) {
+	wfLoadSkin( 'Tempo' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Tempo'] = __DIR__ . '/i18n';
+	/*wfWarn(
+		'Deprecated PHP entry point used for Tempo skin. Please use wfLoadSkin instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);*/
+	return;
+} else {
+	die( 'This version of the Tempo skin requires MediaWiki 1.25+' );
 }
-
-// Skin credits that will show up on Special:Version
-$wgExtensionCredits['skin'][] = array(
-	'path' => __FILE__,
-	'name' => 'Tempo',
-	'version' => '1.6rc1',
-	'author' => array( 'Lojjik Braughler' ),
-	'descriptionmsg' => 'tempo-desc',
-	'license-name' => 'CC0 1.0'
-);
-
-$wgValidSkinNames['tempo'] = 'Tempo';
-
-$wgAutoloadClasses['SkinTempo'] = __DIR__ . '/Tempo.skin.php';
-$wgAutoloadClasses['NotificationsMenuTemplate'] = __DIR__ . '/templates/NotificationsMenu.tmpl.php';
-$wgMessagesDirs['SkinTempo'] = __DIR__ . '/i18n';
-
-// Main CSS ResourceLoader module
-$wgResourceModules['skins.tempo'] = array(
-	'styles' => array(
-		'skins/Tempo/resources/tempo.css' => array( 'media' => 'screen' ),
-		'skins/Tempo/resources/normalize.css' => array( 'media' => 'screen' )
-	),
-	'position' => 'top'
-);
-
-$wgResourceModules['skins.tempo.js'] = array(
-	'scripts' => array(
-		'skins/Tempo/resources/js/tempo.js'
-	)
-);
